0bd4e99a1e8da9a16230a7d250fce6f67bbdb963
Louis SERRANO <SERRANO@GALLIFA11>
2026-03-02 15:40:36 +0100

    feat: Init dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Analytics } from "@vercel/analytics/next"
 import Home from './pages/Home'
 import PayFlow from './pages/PayFlow'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pay/:id" element={<PayFlow />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <Analytics />
     </BrowserRouter>
