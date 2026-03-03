f1e4da1b145d4496982dd324dc44fd7e99c327f6
hemvall <louisserrano777@gmail.com>
2026-03-03 23:21:15 +0100

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { WagmiProvider } from './lib/WagmiProvider'
 import Home from './pages/Home'
 import PayFlow from './pages/PayFlow'
 import Dashboard from './pages/Dashboard'
+import History from './pages/History'
 import Terms from './pages/Terms'
 import HowItWorks from './pages/HowItWorks'
+import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import ThemeToggle from './components/ThemeToggle'
 
@@ -13,13 +15,17 @@ export default function App() {
   return (
     <WagmiProvider>
       <BrowserRouter>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pay/:id" element={<PayFlow />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/history" element={<History />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/how-it-works" element={<HowItWorks />} />
         </Routes>
+        <NavBar />
+        
         <Footer />
         <ThemeToggle />
         <Analytics />
